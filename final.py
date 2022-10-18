0497970316f453ba2cbfe7cc5992b1b7f47339d1
Gabriella Martin <gabriella.martin0101@gmail.com>
2022-10-18 17:54:41 +0100

    Accept google cookies

---
+++ final.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+URL = 'https://www.google.com/search?q=junior+python+jobs&oq=jun&aqs=chrome.0.69i59j69i57j46i131i433i512j46i433i512j0i433i512j69i61j69i60j69i61.819j0j4&sourceid=chrome&ie=UTF-8&ibp=htl;jobs&sa=X&ved=2ahUKEwinz-Xf8en6AhVXPcAKHZhiCYMQutcGKAF6BAgQEAY&sxsrf=ALiCzsaYrhYUO4lOURleqjVQnBtjCSo8lg:1666099887835#htivrt=jobs&fpstate=tldetail&htichips=&htischips=&htidocid=7PLPViTybSQAAAAAAAAAAA%3D%3D'
+driver.get(URL)
+
+time.sleep(2)
+
+def accept_cookies():
+    accept_cookies_button = driver.find_element(by=By.XPATH, value ='//button[@aria-label="Accept all"]')
+    accept_cookies_button.click()
+
+accept_cookies()
