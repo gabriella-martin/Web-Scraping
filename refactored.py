bc4442b6e5cbb140c8501fe642eea20e694684eb
Gabriella Martin <gabriella.martin0101@gmail.com>
2022-10-19 20:14:43 +0100

    clean up data for use in table

--- refactored.py
+++ refactored.py
@@ -11,6 +11,7 @@ class Scraper():
     def __init__(self):
       
         self.job_info = []
+        self.job_info_formatted = []
         self.job_description = []
         self.driver = webdriver.Chrome()
         self.URL = 'https://www.google.com/search?q=junior+%22python%22+&oq=junior&aqs=chrome.0.69i59j46i512j0i131i433i512j46i131i175i199i433i512j0i433i512j69i61j69i60j69i61.1164j0j7&sourceid=chrome&ie=UTF-8&ibp=htl;jobs&sa=X&ved=2ahUKEwj2nObB3uv6AhWMQkEAHSstDrsQutcGKAF6BAgKEAY&sxsrf=ALiCzsYQI5IhM4tuqlfkEd3fJWNNOEUUmw:1666163444131#htivrt=jobs&fpstate=tldetail&htilrad=24.1401&htichips=city:8_MXt1sbdkgKsgA5eS6RSQ%3D%3D&htischips=city;8_MXt1sbdkgKsgA5eS6RSQ%3D%3D:London&htidocid=Ad8ENHx4r_AAAAAAAAAAAA%3D%3D'
@@ -31,34 +32,31 @@ class Scraper():
     def get_job_info(self):
         job_info_list = (self.driver.find_elements(by=By.XPATH, value = '//div[@class="PwjeAc"]'))
         for job in job_info_list:
-            job_text = job.text
+            job_text = job.text.splitlines()
             self.job_info.append(job_text)
         return(self.job_info)
         
 
     def format_job_list(self, full_job_info_list):
         for job in full_job_info_list:
-            list_job_info = job.splitlines()
-            list_job_info = list_job_info[:-1]
-            if len(list_job_info) > 5:
-                list_job_info = list_job_info[1:]
-                print(list_job_info)
+            job = job[:4]
+            if len(job[0]) == 1:
+                job = job[1:]
+                job = job[:4]
+                self.job_info_formatted.append(job)
             else:
-                print(list_job_info)
-    
-    '''def get_description(self):
+                job = job[:4]
+                self.job_info_formatted.append(job)
+                
+
+        print(self.job_info_formatted)
+
+    def get_description(self):
         find_description = (self.driver.find_elements(by=By.XPATH, value = '//span[@class="HBvzbc"]'))
         for description in find_description:
             description_text = description.get_attribute('innerText')
             self.job_description.append(description_text)
 
-  
-    def workbook(self):
-        wb = load_workbook('Job Data.xlsx')
-        ws = wb.active
-        ws.append(self.job_info)
-        wb.save('Job Data.xlsx')'''
-    
     def scrape(self):
         self.accept_cookies()
         self.specify_location()
